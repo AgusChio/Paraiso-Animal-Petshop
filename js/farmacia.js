377c0814ad62b5b2d6ac87112e9e891d1e2d5001
Agustina Chiodini <agus.chio25790@gmail.com>
2023-03-29 16:50:12 -0300

    todo listo

---
+++ js/farmacia.js
@@ -0,0 +1,108 @@
+const { createApp } = Vue
+
+const app = createApp({
+    data() {
+        return {
+            medicamentos: [],
+            medicamentosFiltrados: [],
+            textoInput: '',
+            seleccionada: '',
+            carrito: [],
+            total: 0
+        }
+    },
+    created() {
+        fetch('https://mindhub-xj03.onrender.com/api/petshop')
+            .then(response => response.json())
+            .then(data => {
+                this.obtenerMedicamentos(data.response)
+            })
+            .catch(err => console.log(err))
+        this.carrito = JSON.parse(localStorage.getItem('carrito'))
+        if (!this.carrito) {
+            this.carrito = []
+        } else {
+            this.carrito.forEach(medicamento => this.total += medicamento.unidades*medicamento.precio)
+        }
+    },
+    methods: {
+        obtenerMedicamentos(data) {
+            this.medicamentos = data.filter(elemento => elemento.tipo === "Medicamento")
+            this.medicamentos = this.medicamentos.sort((a, b) => a.nombre.localeCompare(b.nombre))
+            this.medicamentosFiltrados = this.medicamentos
+            console.log(this.medicamentosFiltrados)
+        },
+        filtrarMedicamentos() {
+            console.log(this.textoInput)
+            if (this.textoInput) {
+                this.medicamentosFiltrados = this.medicamentos.filter(medicamento => {
+                    return medicamento.nombre.toLowerCase().trim().includes(this.textoInput.toLowerCase().trim())
+                })
+                /* this.medicamentosFiltrados = this.medicamentos.filter(medicamento => medicamento.stock > 5) */
+            } else {
+                this.medicamentosFiltrados = this.medicamentos
+            }
+        },
+        agregarAlCarrito(medicamento) {
+            if (this.carrito.find(med => med._id === medicamento._id)) {
+                const index = this.carrito.findIndex(med => med._id === medicamento._id)
+                if(this.carrito[index].unidades < this.carrito[index].stock)
+                    this.carrito[index].unidades += 1
+            } else {    
+                medicamento.unidades = 1
+                this.carrito.push(medicamento)
+            }
+            this.total += medicamento.precio
+            localStorage.setItem('carrito', JSON.stringify(this.carrito))
+        },
+        quitarUnidad(medicamento) {
+            const index = this.carrito.findIndex(med => med._id === medicamento._id)
+            this.carrito[index].unidades -= 1
+            if (!this.carrito[index].unidades) {
+                this.carrito.splice(index,1)
+            }
+            this.total -= medicamento.precio
+            localStorage.setItem('carrito', JSON.stringify(this.carrito))
+        },
+        quitarElemento(medicamento) {
+            const index = this.carrito.findIndex(med => med._id === medicamento._id)
+            this.carrito.splice(index, 1)
+            localStorage.setItem('carrito', JSON.stringify(this.carrito))
+        }
+    },
+    computed: {
+        filtrar() {
+            let ordenarPor;
+            switch (this.seleccionada) {
+                case 'A-Z':
+                    this.medicamentosFiltrados = this.medicamentosFiltrados.sort((a, b) => a.nombre.localeCompare(b.nombre))
+                    break;
+                case 'Mayor precio':
+                    this.medicamentosFiltrados = this.medicamentosFiltrados.sort((a, b) => b.precio - a.precio)
+                    break;
+                case 'Menor precio':
+                    this.medicamentosFiltrados = this.medicamentosFiltrados.sort((a, b) => a.precio - b.precio)
+                    break;
+                default:
+                    this.medicamentosFiltrados = this.medicamentosFiltrados
+            }
+            
+        },
+        cantTotal: function(){
+            let cant = 0;
+            for(key in this.carrito){
+                cant = cant + this.carrito[key].unidades 
+            }
+            return cant
+        },
+        carritoTotal: function(){
+            let suma = 0;
+            for(key in this.carrito){
+                suma = suma +(this.carrito[key].unidades * this.carrito[key].precio)
+            }
+            return suma
+        },
+    }
+})
+
+app.mount('#app')
---
+++ js/juguetes.js
@@ -0,0 +1,108 @@
+const { createApp } = Vue
+
+const app = createApp({
+    data() {
+        return {
+            juguetes: [],
+            juguetesFiltrados: [],
+            textoInput: '',
+            seleccionada: '',
+            carrito: [],
+            total: 0
+        }
+    },
+    created() {
+        fetch('https://mindhub-xj03.onrender.com/api/petshop')
+            .then(response => response.json())
+            .then(data => {
+                this.obtenerJuguetes(data.response)
+            })
+            .catch(err => console.log(err))
+            this.carrito = JSON.parse(localStorage.getItem('carrito'))
+            if (!this.carrito) {
+                this.carrito = []
+            } else {
+                this.carrito.forEach(medicamento => this.total += medicamento.unidades*medicamento.precio)
+            }
+    },
+    methods: {
+        obtenerJuguetes(data) {
+            this.juguetes = data.filter(elemento => elemento.tipo === "Juguete")
+            this.juguetes = this.juguetes.sort((a, b) => a.nombre.localeCompare(b.nombre))
+            this.juguetesFiltrados = this.juguetes
+            console.log(this.juguetesFiltrados)
+        },
+        filtrarjuguetes() {
+            console.log(this.textoInput)
+            if (this.textoInput) {
+                this.juguetesFiltrados = this.juguetes.filter(Juguete => {
+                    return Juguete.nombre.toLowerCase().trim().includes(this.textoInput.toLowerCase().trim())
+                })
+            } else {
+                this.juguetesFiltrados = this.juguetes
+            }
+        },
+        agregarAlCarrito(Juguete) {
+            if (this.carrito.find(jug => jug._id === Juguete._id)) {
+                const index = this.carrito.findIndex(jug => jug._id === Juguete._id)
+                if(this.carrito[index].unidades < this.carrito[index].stock)
+                    this.carrito[index].unidades += 1
+            } else {    
+                Juguete.unidades = 1
+                this.carrito.push(Juguete)
+            }
+            this.total += Juguete.precio
+            localStorage.setItem('carrito', JSON.stringify(this.carrito))
+        },
+        quitarUnidad(Juguete) {
+            const index = this.carrito.findIndex(med => med._id === Juguete._id)
+            this.carrito[index].unidades -= 1
+            if (!this.carrito[index].unidades) {
+                this.carrito.splice(index,1)
+            }
+            this.total -= Juguete.precio
+            localStorage.setItem('carrito', JSON.stringify(this.carrito))
+        },
+        quitarElemento(Juguete) {
+            const index = this.carrito.findIndex(med => med._id === Juguete._id)
+            this.carrito.splice(index, 1)
+            localStorage.setItem('carrito', JSON.stringify(this.carrito))
+            this.total -= Juguete.precio * Juguete.unidades
+        }
+    },
+    computed: {
+        filtrar() {
+            let ordenarPor;
+            switch (this.seleccionada) {
+                case 'A-Z':
+                    this.juguetesFiltrados = this.juguetesFiltrados.sort((a, b) => a.nombre.localeCompare(b.nombre))
+                    break;
+                case 'Mayor precio':
+                    this.juguetesFiltrados = this.juguetesFiltrados.sort((a, b) => b.precio - a.precio)
+                    break;
+                case 'Menor precio':
+                    this.juguetesFiltrados = this.juguetesFiltrados.sort((a, b) => a.precio - b.precio)
+                    break;
+                default:
+                    this.juguetesFiltrados = this.juguetesFiltrados
+            }
+            
+        },
+        cantTotal: function(){
+            let cant = 0;
+            for(key in this.carrito){
+                cant = cant + this.carrito[key].unidades 
+            }
+            return cant
+        },
+        carritoTotal: function(){
+            let suma = 0;
+            for(key in this.carrito){
+                suma = suma +(this.carrito[key].unidades * this.carrito[key].precio)
+            }
+            return suma
+        },
+    }
+})
+
+app.mount('#app')
